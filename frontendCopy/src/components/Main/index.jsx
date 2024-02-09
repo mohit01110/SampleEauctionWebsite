0f48cc88e7272da7333a3fb3df3f2df41ea11687
Mohit Choudhary <132933699+mohit01110@users.noreply.github.com>
2024-02-09 19:15:08 +0530

    Add files via upload

---
+++ frontendCopy/src/components/Main/index.jsx
@@ -0,0 +1,28 @@
+import styles from './style.module.css';
+import img from '../../image/food.jpeg'
+
+const Main = () => {
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    window.location.reload();
+  };
+  return (
+    <div className={styles.vic}>
+    <h2>Successful
+    </h2>
+    <navbar>
+      <button className={styles.white_btn} onClick={handleLogout}>
+        <h3>Logout</h3>
+      </button>      
+      </navbar>
+      <div className={styles.foto}>
+        <img 
+        src={img} 
+        alt="img" 
+        />
+        </div> 
+      </div>
+  )
+}
+
+export default Main;
